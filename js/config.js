3cd7896b66b482d6a69fbd6d771d018261424064
banglegen <bawngbui46@gmail.com>
2026-09-10 09:10:02 +0700

    3

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@
   3. Điền URL và anon key bên dưới.
   4. Chạy file supabase/schema.sql trong SQL Editor.
 */
-var SUPABASE_URL = "";
-var SUPABASE_ANON_KEY = "";
+var SUPABASE_URL = "https://yoalbkrfqwbbhqyeywns.supabase.co/rest/v1/";
+var SUPABASE_ANON_KEY = "sb_publishable_O5QTQnlLlPoVHAe46-kRlg_ST8itddP";
 
 /* Nếu chưa điền Supabase, app dùng localStorage để test giao diện. */
 var USE_SUPABASE = !!(SUPABASE_URL && SUPABASE_ANON_KEY);
